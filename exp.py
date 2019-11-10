c94af27be391b451eed922324f275ae73695fa71
Alex Yu <alexyu@s-164-67-3-30.resnet.ucla.edu>
2019-11-10 11:31:27 -0800

    Added labeling

--- exp.py
+++ exp.py
@@ -1,111 +1,134 @@
 from psychopy import visual, event, core, logging, gui
 import os
+import time
 import numpy as np
 import pandas as pd
-import time
 
-def get_random_img(img_list):
-    rand_index = np.random.randint(0, len(img_list))
-    img = img_list[rand_index]
-    img_list.remove(img)
-    return img
-
-window_dims = [600,600]                                 # dimensions of window display
-key_list = ['y', 'n']                                   # options for user response
-time_to_show = 0.200                                    # time for image to be displayed in seconds
-
+# CONSTANTS
 '''
 Columns in the .csv file containing raw experiment data:
 Image - image filename shown
-Response - user key input to prompt
+Response - user key input to prompt ('y' for yes and 'n' for no)
 Actual - actual/expected answer to prompt
 Time - response time (in seconds) defined as time between image display and user response
 '''
 data_columns = ['Image', 'Response', 'Actual', 'Time']
 
-img_dir = r"images/"                                    # directory containing images to display
-img_list = [f for f in os.listdir(img_dir)]
+window_dims = [600,600]                       # dimensions of window display
+key_list = ['y', 'n']                         # options for user response
+time_to_show = 0.200                          # time for image to be displayed in seconds
+img_dir = r"images/"                          # directory containing images to display
 
-# Sets system time as the random seed
-np.random.seed(seed=None)
+damage_subdir = "Damage"                      # subdirectory name to images of damaged buildings
+nodamage_subdir = "NoDamage"                  # subdirectory name to images of undamged buildings
 
-# Prompt GUI for participant ID
-dlg = gui.Dlg(title="Enter participant ID:")
-dlg.addField('Participant ID:')
-id = dlg.show()
-if not dlg.OK:
-    print('Program aborted.')
-    exit()
-id = id[0]
+# UTILITY FUNCTIONS
+def get_imgs(img_dir, damage_subdir, nodamage_subdir):
+    imgs_list = []
+    imgs_list.extend([f"{damage_subdir}/" + f for f in os.listdir(img_dir + damage_subdir)])
+    imgs_list.extend([f"{nodamage_subdir}/" + f for f in os.listdir(img_dir + nodamage_subdir)])
+    return imgs_list
 
-# Set up window and how many frames image should be displayed depending on refresh rate of monitor
-window = visual.Window(size=window_dims, monitor='monitor')
-frame_rate = window.getActualFrameRate()
-frames_to_show = (int) (round(time_to_show * frame_rate))
+def parse_img(img_file):
+    slash = img_file.find('/')
+    return img_file[:slash], img_file[slash+1:]
 
-instruction_msg = visual.TextStim(window, text="Each image will be shown briefly after a 3 second countdown.")
-instruction_msg.draw()
-window.flip()
-core.wait(3)
+def get_random_img(img_list):
+    rand_index = np.random.randint(0, len(img_list))
+    img = img_list[rand_index]
+    img_list.remove(img)
+    return parse_img(img)
 
-instruction_msg = visual.TextStim(window, text="Is the building damaged?\n\nPlease press 'y' for yes and 'n' for no.")
-countdown_msg = visual.TextStim(window, text=None)
-img = visual.ImageStim(window)
+# MAIN ROUTINE
+def main():
+    img_list = get_imgs(img_dir, damage_subdir, nodamage_subdir)
+    print(f"{len(img_list)} images loaded.")
 
-data = pd.DataFrame(columns=data_columns)
+    # Sets system time as the random seed
+    np.random.seed(seed=None)
 
-# Run through image list with participant
-while len(img_list) > 0:
+    # Prompt GUI for participant ID
+    dlg = gui.Dlg(title="Enter participant ID:")
+    dlg.addField('Participant ID:')
+    id = dlg.show()
+    if not dlg.OK:
+        print('Program aborted.')
+        exit()
+    id = id[0]
 
-    # Shows 3 second countdown to image display
-    for n in range(3, 0, -1):
-        countdown_msg.text = f"{n}"
-        countdown_msg.draw()
-        window.flip()
-        core.wait(1.0)
+    # Set up window and how many frames image should be displayed depending on refresh rate of monitor
+    window = visual.Window(size=window_dims, monitor='monitor')
+    frame_rate = window.getActualFrameRate()
+    frames_to_show = (int) (round(time_to_show * frame_rate))
 
-    img_name = get_random_img(img_list)
-    img.setImage(img_dir + img_name)
-
-    start_time = time.time()
-    for frame in range(frames_to_show):
-        img.draw()
-        window.flip()
-
+    instruction_msg = visual.TextStim(window, text="Each image will be shown briefly after a 3 second countdown.")
     instruction_msg.draw()
     window.flip()
+    core.wait(3)
 
-    # Track reaction time for user response
-    answer = event.waitKeys(keyList=key_list)
-    end_time = time.time()
-    reaction_time = end_time - start_time
+    instruction_msg = visual.TextStim(window, text="Is the building damaged?\n\nPlease press 'y' for yes and 'n' for no.")
+    countdown_msg = visual.TextStim(window, text=None)
+    img = visual.ImageStim(window)
 
-    #TODO: import response
-    data.loc[len(data)] = ([img_name, answer[0], '', reaction_time])
-    # logging.log(level=logging.DATA, msg=f"The key pressed for the picture {img_name} was {answer[0]}."))
+    data = pd.DataFrame(columns=data_columns)
 
-instruction_msg.text = "Test completed. Closing window..."
-instruction_msg.draw()
-window.flip()
-core.wait(3.0)
-window.close()
+    # Run through image list with participant
+    while len(img_list) > 0:
 
-# Output individual participant data to .csv file
-if not os.path.exists('data'):
-    os.makedirs('data')
-data.to_csv(f"data/data_{id}.csv")
+        # Show 3 second countdown to image display
+        for n in range(3, 0, -1):
+            countdown_msg.text = f"{n}"
+            countdown_msg.draw()
+            window.flip()
+            core.wait(1.0)
 
-# Output summarized participant data to summary file
-accuracy = len(data.query('Response == Actual')) / len(data)
+        # Get and parse random image's information
+        subdir, img_name = get_random_img(img_list)
+        truth = 'y' if subdir == damage_subdir else 'n'
+        img.setImage(img_dir + subdir + '/' + img_name)
 
-if not os.path.exists('summary.csv'):
-    summary = pd.DataFrame(columns=['ID', 'Accuracy'])
-    summary.to_csv('summary.csv')
+        # Display the image for set number of frames
+        start_time = time.time()
+        for _ in range(frames_to_show):
+            img.draw()
+            window.flip()
 
-index = 0
-with open('summary.csv') as file:
-    index = sum(1 for line in file)
+        instruction_msg.draw()
+        window.flip()
 
-with open('summary.csv', 'a') as file:
-    file.write(f"{index},{id},{accuracy}")
-    file.write("\n")
+        # Track reaction time for user response
+        answer = event.waitKeys(keyList=key_list)
+        end_time = time.time()
+        reaction_time = end_time - start_time
+
+        data.loc[len(data)] = ([img_name, answer[0], truth, reaction_time])
+
+    instruction_msg.text = "Test completed. Closing window..."
+    instruction_msg.draw()
+    window.flip()
+    core.wait(3.0)
+    window.close()
+
+    # Output individual participant data to .csv file
+    if not os.path.exists('data'):
+        os.makedirs('data')
+    data.to_csv(f"data/data_{id}.csv")
+
+    # Create summary file if it doesn't already exist
+    if not os.path.exists('summary.csv'):
+        summary = pd.DataFrame(columns=['ID', 'Accuracy'])
+        summary.to_csv('summary.csv')
+
+    # Output summarized participant data to summary file
+    accuracy = len(data.query('Response == Actual')) / len(data)
+
+    index = 0
+    with open('summary.csv') as file:
+        index = sum(1 for line in file)
+
+    with open('summary.csv', 'a') as file:
+        file.write(f"{index},{id},{accuracy}")
+        file.write("\n")
+
+if __name__ == "__main__":
+    main()
